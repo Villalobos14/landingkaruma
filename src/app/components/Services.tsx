a8ab58621efabf25e8cbf6a94c33d608e9bbe19e
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2025-03-28 15:13:01 -0600

    hola

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -39,7 +39,7 @@ export default function Services() {
                     <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-primary bg-primary shadow-2xl">
                       <Image
                         className="size-full object-cover object-top"
-                        src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
+                        src="/images/iphone.png"
                         alt=""
                         draggable="false"
                         width={600}
@@ -54,9 +54,9 @@ export default function Services() {
                 <div className="absolute inset-px rounded-lg bg-primary-light opacity-30 backdrop-blur-2xl max-lg:rounded-t-[2rem]"></div>
                 <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                   <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                    <p className="mt-2 text-lg font-medium tracking-tight text-complementary max-lg:text-center">E-commerce</p>
+                    <p className="mt-2 text-lg font-medium tracking-tight text-complementary max-lg:text-center">Optimización SEO</p>
                     <p className="mt-2 max-w-lg text-sm/6 text-complementary max-lg:text-center">
-                      Diseñamos y desarrollamos tiendas en línea que convierten visitantes en clientes.
+                      Aumentamos tu visibilidad en los motores de búsqueda para atraer más clientes potenciales.
                     </p>
                   </div>
                   <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
@@ -73,9 +73,9 @@ export default function Services() {
                 <div className="absolute inset-px rounded-lg bg-primary-light opacity-30 backdrop-blur-2xl"></div>
                 <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
                   <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                    <p className="mt-2 text-lg font-medium tracking-tight text-complementary max-lg:text-center">Branding y Diseño UX/UI</p>
+                    <p className="mt-2 text-lg font-medium tracking-tight text-complementary max-lg:text-center">Desarrollo Blockchain</p>
                     <p className="mt-2 max-w-lg text-sm/6 text-complementary max-lg:text-center">
-                      Construimos marcas sólidas y experiencias digitales atractivas para conectar con tu audiencia.
+                      Desarrollamos soluciones blockchain innovadoras para optimizar procesos y potenciar tu negocio.
                     </p>
                   </div>
                   <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
@@ -93,24 +93,22 @@ export default function Services() {
                 <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
                   <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
                     <p className="mt-2 text-lg font-medium tracking-tight text-complementary max-lg:text-center">
-                    Optimización SEO
+                      Branding y Diseño UX/UI
                     </p>
                     <p className="mt-2 max-w-lg text-sm/6 text-complementary max-lg:text-center">
-                      Aumentamos tu visibilidad en los motores de búsqueda para atraer más clientes potenciales.
+                      Construimos marcas sólidas y experiencias digitales atractivas para conectar con tu audiencia.
                     </p>
                   </div>
-                  <div className="relative min-h-[30rem] w-full grow">
-                    <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-primary shadow-2xl">
-                      <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                        <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                          <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                            NotificationSetting.jsx
-                          </div>
-                          <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                        </div>
-                      </div>
-                      <div className="px-6 pt-6 pb-14">
-                      </div>
+                  <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
+                    <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-primary bg-primary shadow-2xl">
+                      <Image
+                        className="size-full object-cover object-top"
+                        src="/images/iphone2.png"
+                        alt=""
+                        draggable="false"
+                        width={600}
+                        height={600}
+                      />
                     </div>
                   </div>
                 </div>
